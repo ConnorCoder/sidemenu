cec53135952002ac14be26d36502821b9585637f
ConnorCoder <43045469+ConnorCoder@users.noreply.github.com>
2020-01-27 07:32:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let corner = 0;
 let corners = ["Top Left", "Top Right", "Bottom Left", "Bottom Right"];
 let cornersty = ["top:0px;left:0px;","top:0px;right:0px;","bottom:0px;left:0px;","bottom:0px;right:0px;"];
+let side = "Right";
 
 function loop() {
   let width = g("sb-width").value;
@@ -12,8 +13,9 @@ function loop() {
   g("sb-activate-height-txt").innerHTML = "Activation Button Height (" + aheight + "px):"
   g("sb-activate-display").style.width = awidth + "px";
   g("sb-activate-display").style.height = aheight + "px";
+  g("sb-side").innerHTML = "Side (" + side + "):";
   
-  document.getElementById("sb-output").value = 'javascript:let toggle = 0;let ele = document.createElement("button");ele.style = "background:transparent;color:transparent;border:none;width:' + awidth + "px;height:" + aheight + 'px;position:fixed;z-Index:10000;' + cornersty[corner] + '";ele.onclick=function(){if(toggle === 0) {document.getElementById("divhack").style.display=null;toggle=1}else {document.getElementById("divhack").style.display="none";toggle=0};};document.body.appendChild(ele);let div = document.createElement("div");div.style = "width:' + width + '%;height:100%;position:fixed;right:0px;top:0px;z-Index:5000;color:black;background:white;border-color:black;border-left:solid;border-width:3px;display:none";div.id="divhack";document.body.appendChild(div);let txt = document.createElement("p");txt.innerHTML="' + document.getElementById("sb-txt").value.split(`
+  document.getElementById("sb-output").value = 'javascript:let toggle = 0;let ele = document.createElement("button");ele.style = "background:transparent;color:transparent;border:none;width:' + awidth + "px;height:" + aheight + 'px;position:fixed;z-Index:10000;' + cornersty[corner] + '";ele.onclick=function(){if(toggle === 0) {document.getElementById("divhack").style.display=null;toggle=1}else {document.getElementById("divhack").style.display="none";toggle=0};};document.body.appendChild(ele);let div = document.createElement("div");div.style = "width:' + width + '%;height:100%;position:fixed;' + side + ':0px;top:0px;z-Index:5000;color:black;background:white;border-color:black;border-left:solid;border-width:3px;display:none";div.id="divhack";document.body.appendChild(div);let txt = document.createElement("p");txt.innerHTML="' + document.getElementById("sb-txt").value.split(`
 `).join("<br>").split('"').join('\\"') + '";document.getElementById("divhack").appendChild(txt);void 0';
 
 }setInterval(loop, 0);
@@ -21,5 +23,13 @@ function g(id) {
   return document.getElementById(id);
 }
 function select(x) {
-  corner = x;
+  if(x < 4) {
+    corner = x;
+  }else if (x < 6) {
+    if (x === 4) {
+      side = "Left";
+    }else {
+      side = "Right";
+    }
+  }
 }
