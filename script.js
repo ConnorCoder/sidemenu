ed3ceb8e29aa87d4f429db3d76ead22e3c25a840
ConnorCoder <43045469+ConnorCoder@users.noreply.github.com>
2020-01-27 07:37:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,11 @@ function loop() {
   g("sb-activate-display").style.height = aheight + "px";
   g("sb-side").innerHTML = "Side (" + side + "):";
   
-  document.getElementById("sb-output").value = 'javascript:let toggle = 0;let ele = document.createElement("button");ele.style = "background:transparent;color:transparent;border:none;width:' + awidth + "px;height:" + aheight + 'px;position:fixed;z-Index:10000;' + cornersty[corner] + '";ele.onclick=function(){if(toggle === 0) {document.getElementById("divhack").style.display=null;toggle=1}else {document.getElementById("divhack").style.display="none";toggle=0};};document.body.appendChild(ele);let div = document.createElement("div");div.style = "width:' + width + '%;height:100%;position:fixed;' + side + ':0px;top:0px;z-Index:5000;color:black;background:white;border-color:black;border-left:solid;border-width:3px;display:none";div.id="divhack";document.body.appendChild(div);let txt = document.createElement("p");txt.innerHTML="' + document.getElementById("sb-txt").value.split(`
+  let script = 'javascript:let toggle = 0;let ele = document.createElement("button");ele.style = "background:transparent;color:transparent;border:none;width:' + awidth + "px;height:" + aheight + 'px;position:fixed;z-Index:10000;' + cornersty[corner] + '";ele.onclick=function(){if(toggle === 0) {document.getElementById("divhack").style.display=null;toggle=1}else {document.getElementById("divhack").style.display="none";toggle=0};};document.body.appendChild(ele);let div = document.createElement("div");div.style = "width:' + width + '%;height:100%;position:fixed;' + side + ':0px;top:0px;z-Index:5000;color:black;background:white;border-color:black;border-left:solid;border-width:3px;display:none";div.id="divhack";document.body.appendChild(div);let txt = document.createElement("p");txt.innerHTML="' + g("sb-txt").value.split(`
 `).join("<br>").split('"').join('\\"') + '";document.getElementById("divhack").appendChild(txt);void 0';
+  
+  document.getElementById("sb-output").value = script;
+  g("dragme").href = script;
 
 }setInterval(loop, 0);
 function g(id) {
